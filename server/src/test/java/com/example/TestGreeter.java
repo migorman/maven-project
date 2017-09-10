92ab7cf24a3f890b60235539cb8e5fe034033533
migorman <mounir.igorman@gmail.com>
2017-09-10 20:58:10 +0100

    unit test success

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -21,7 +21,7 @@ public class TestGreeter {
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
 
-    assertThat(greeter.greet("haha"), containsString(someone));
+    assertThat(greeter.greet(someone), containsString(someone));
   }
 
   @Test
